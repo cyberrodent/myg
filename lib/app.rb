0c2b3aaccd5ba6ce8171cc539f0652992dcaf3e7
CyberRodent <fej.reblok@gmail.com>
2013-02-01 02:32:04 +0000

    move some things around in parse

--- lib/app.rb
+++ lib/app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'mustache/sinatra'
 
 module Mygoogle
+
     class App < Sinatra::Base
         register Mustache::Sinatra
 
@@ -16,7 +17,6 @@ module Mygoogle
         set :public_folder, "public/"
         set :static, true
 
-
         before do
             # setup()
             # puts "before"
@@ -29,50 +29,53 @@ module Mygoogle
         end
 
         get '/parse' do
+
             start_time = Time.now
 
             num_feeds = 0 
-
-            tabs = parsePrefs()
             tabs_parse = []
 
+            tabs = parsePrefs()
             tabs.each {|tab|
 
+                tname = tab[:tabname]
                 tab_temp = []
-
-                tname = tab[:tabname] 
+                tab_feeds = 0
+                
                 $logger.info("Fetch RSS feeds in #{tname}")
 
-                tab_feeds = 0
                 tab[:tabrss].each {|rss|
 
                     # TODO
                     break if tab_feeds > 10
                     # break if num_feeds > 0
-                    tab_feeds = tab_feeds + 1
 
                     $logger.info("\tFetching #{rss}")
+
                     res = fetchFeed(rss)
 
-                    feed_title = res.nil? ? "untitled" : res.title
+                    tab_feeds = tab_feeds + 1
                     num_feeds = num_feeds + 1
+
+                    feed_title = res.nil? ? "untitled" : res.title
+
                     pfeed = processFeed(res) 
 
                     tab_temp << { 
                         :feed_title => feed_title,
                         :feed_data  => pfeed 
                     }
-                }
-                tabs_parse << {
+                } # end of each tabrss
+
+                tabs_parse << { 
                     :tab_name => tname,
                     :tab_data => tab_temp
-                    }
-            }
+                }
+            } # end of all tabs
 
             duration = Time.now - start_time
             $logger.info("Parsed #{num_feeds} feed; took #{duration} seconds")
 
-            # return tabs_parse.inspect
             @tabs_parse = tabs_parse
 
             mustache :parse 
