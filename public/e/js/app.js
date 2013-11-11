a69a7ad9b8b0692edcb76281f2d52aab9cf7fec6
CyberRodent <fej.reblok@gmail.com>
2013-11-11 10:52:39 -0500

    moving the ember front end into this project

---
+++ public/e/js/app.js
@@ -0,0 +1,34 @@
+App = Ember.Application.create( {
+  LOG_TRANSITIONS: true
+});
+
+App.Router.map(function() {
+  this.route("settings", { path: "/settings" });
+  this.route("tab", { path: "/tab/:tabname" });
+});
+
+App.IndexRoute = Ember.Route.extend({
+    model: function () { 
+        var tabs_url = "/tabs/list";
+        return Ember.$.getJSON(tabs_url);
+    }
+});
+
+App.SettingsRoute = Ember.Route.extend({
+    model: function () { return [ 
+        { "name" : "AAA" },
+        { "name" : "BBB" },
+        { "name" : "CCC" }
+    ];
+    }
+});
+
+App.TabRoute = Ember.Route.extend({
+    setupController: function (controller, model) {
+        controller.set('model', model);
+    },
+    model: function (params) { 
+        var tabs_url = "/tabdata/"+ params.tabname.toLowerCase();
+        return Ember.$.getJSON(tabs_url);
+    }
+});
