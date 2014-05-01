be5eb676e2dcaf52bac8a72c567e645aa8e2411f
CyberRodent <fej.reblok@gmail.com>
2014-05-01 01:12:59 +0000

    still trying to get something to work

--- public/e/js/app.js
+++ public/e/js/app.js
@@ -1,27 +1,8 @@
 /*
  * Setup
  */
-String.prototype.hashCode = function(){
-    var hash = 0, i, char;
-    if (this.length == 0) return hash;
-    for (i = 0, l = this.length; i < l; i++) {
-        char  = this.charCodeAt(i);
-        hash  = ((hash<<5)-hash)+char;
-        hash |= 0; // Convert to 32bit integer
-    }
-    return hash;
-};
 
-
-
-
-
-
-
-
-/* 
- * Start the App
- */
+/* Start the App */
 App = Ember.Application.create( {
 //    LOG_STACKTRACE_ON_DEPRECATION : true,
         LOG_BINDINGS                  : true,
@@ -33,11 +14,13 @@ App = Ember.Application.create( {
 });
 
 
+
 App.Router.map(function() {
 
     this.resource("index", { path: "/" }, function () {
         this.resource("tab", { path: "/tab/:tabname" }, function () {
             this.resource("article", { path: "/article/:article_id" });
+            this.resource("feed", { path: "/feed/:feed_id" });
         } );
     } );
 
@@ -56,8 +39,9 @@ App.adapter = DS.Adapter.extend({
     serializer: App.ApplicationSerializer
 });
 
+App.store = DS.Store.extend({
 
-App.store = DS.Store.extend({});
+});
 
 var attr = DS.attr;
 /// Feed Model
@@ -65,6 +49,8 @@ App.Feed = DS.Model.extend({
     feed_title: attr(),
     feed_data: attr()
 });
+
+
 /// Article Model
 App.Article = DS.Model.extend({
     pubdate : attr(),
@@ -81,7 +67,7 @@ App.Article = DS.Model.extend({
 
 /// IndexRoute
 App.IndexRoute = Ember.Route.extend({
-    model: function () { 
+    model: function () {
         var tabs_url = "/tabs/list";
         return Ember.$.getJSON(tabs_url);
     }
@@ -92,7 +78,7 @@ App.IndexRoute = Ember.Route.extend({
 
 /// SettingsRoute
 App.SettingsRoute = Ember.Route.extend({
-    model: function () { return [ 
+    model: function () { return [
         { "name" : "AAA" },
         { "name" : "BBB" },
         { "name" : "CCC" }
@@ -100,105 +86,103 @@ App.SettingsRoute = Ember.Route.extend({
     }
 });
 
-
-
-
-
-
-
+App.FeedslistView = Ember.View.extend({
+    templateName: 'flistview',
+    flist: []
+});
 
 /// TabController
-App.TabController = Ember.ObjectController.extend({ });
+App.TabController = Ember.ObjectController.extend({
+    needs: "feedlist",
+    feeds : function () {
+        return App.Feed.find();
+    }
+});
 
 /// TabRoute
-
-App.TabRouteBuilder = function (params) {
-    var tabs_url = "/tabdata/"+ params.tabname.toLowerCase();
-    var back = Ember.$.getJSON(tabs_url, function (indata) {
-
-        var i = 0;
-        var data = indata[0];
-        var j = data.tab_data.length;
-
-        for (i=0; i<j; i++) {
-            var feed = data.tab_data[i];
-            console.log("Checking " + feed.id);
-            var newfeed = App.store.createRecord('feed', feed);
-            var feed_len = feed.feed_data.length;
-            for (ii = 0; ii < feed_len; ii++) {
-                var article = feed.feed_data[ii];
-                console.log(article.title);
-                var newarticle = App.store.createRecord('article', article);
-            }
-        }
-        // var ret = App.store.find('article', {title : params.title} );
-        // console.log("returning");
-        // console.log(ret.get('title'));
-        // return ret;
-    });
-    return indata;
-};
-
 App.TabRoute = Ember.Route.extend({
     setupController: function (controller, model) {
-        console.log('TabRoute controller');
         controller.set('model', model);
     },
-    model: function (params) { 
+    afterModel: function (mm, tt) {
+      console.log(mm);
+    },
+    model: function (params) {
         // App.TabRouteBuilder(params);
         var tabs_url = "/tabdata/"+ params.tabname.toLowerCase();
-
         var self = this;
-        var back = Ember.$.getJSON(tabs_url, function (indata) {
 
-            var i = 0;
-            var data = indata[0];
-            var j = data.tab_data.length;
+        var back = Ember.$.getJSON(tabs_url, function (indata) {
+            self.store.unloadAll('feed');
+            var i = 0,
+              ii = 0,
+              data = indata[0],
+              j = data.tab_data.length;
 
             for (i=0; i<j; i++) {
                 var feed = data.tab_data[i];
-                // console.log("Checking " + feed.id);
-                var newfeed = self.store.push('feed', feed);
-
                 var feed_len = feed.feed_data.length;
+
+                self.store.push('feed', feed);
                 for (ii = 0; ii < feed_len; ii++) {
-                    var article = feed.feed_data[ii];
-                    // console.log("Storing: " + article.title);
-                    var newarticle = self.store.push('article', article);
+                    self.store.push('article', feed.feed_data[ii]);
                 }
             }
-
-            
         });
+        back.then( function (e) {
+            // console.log(e[0]);
+            var a = self.store.all('feed');
+            var feed_array = a.content.map(function(e) { return e.get('feed_title'); });
+            console.log(feed_array);
+            self.feed_array = feed_array;
+        } );
         return back;
     },
 //    afterModel: function() { this.transitionTo('article', this.model().id); }
 });
 
+
+App.FeedRoute = Ember.Route.extend({
+    tab: Ember.computed.alias("controllers.tab"),
+    model: function (params) {
+      console.log("feed route");
+      var r = this.store.all('feed');
+      console.log(r);
+      return r;
+
+    }
+
+});
+App.FeedController = Ember.ObjectController.extend({
+  needs: "tab"
+});
+
+App.FeedlistController = Ember.ObjectController.extend({
+  needs: "tab",
+  feedlist: ['qweqweqwe','asdfasdfa','xzxcvzxcv']
+});
+
 /// ArticleController
-App.ArticleController = Ember.ObjectController.extend({ 
+App.ArticleController = Ember.ObjectController.extend({
     needs: "tab"
 });
 
 
 /// ArticleRoute
 App.ArticleRoute = Ember.Route.extend({
-   // serialize : function(model) { return { article_id: model.get('id')}; },
-
     setupController: function(controller, model) {
          controller.set('model', model);
-         // console.log("setup article controller"); console.log(model);
      },
-//     model:  function (params) { // console.log(params);
-//         var ret = this.store.find('article', {id: params.article_id} );
-//         if (ret) {
-//             return ret;
-//         } else {
-//             console.log("MAYDAY");
-//             return {};
-//         }
-//     }
  });
 
 
 
+
+
+// Ember.Application.initializer({
+//   name: 'feedme',
+//
+//   initialize: function(container, application) {
+//     application.register('feedme:main', App.FeedlistController);
+//   }
+// });
